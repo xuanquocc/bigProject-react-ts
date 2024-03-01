944b8deef454881a9c3bb68c15f9f6723961160c
xuanquocc <quocntx.21it@vku.udn.vn>
2024-03-01 22:06:32 +0700

    feat:config

---
+++ client/src/assets/themes/colors.ts
@@ -0,0 +1,22 @@
+import {createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2A85FF",
+      contrastText: "#FCFCFCBF",
+    },
+    grey: {
+      500: "#F4F4F4",
+      600: "#9A9FA5",
+    },
+    error: {
+      main: "#FFBC9940",
+      contrastText: "#FF6A55",
+    },
+
+
+  }
+});
+
+export default theme
